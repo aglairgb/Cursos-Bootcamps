9ee06c14cb64aff4a8691bbff4435c71f1cc3ebf
Vittorya <aglairgb@hotmail.com>
2022-12-27 21:43:02 -0400

    anotacoes primeiros passos com js

---
+++ Primeiros passos com JavaScript/Variáveis e operadores/dinâmica/aula.js
@@ -0,0 +1,18 @@
+/*
+Variável, ou seja, varia, portanto, a última alteração que irá valer.
+
+let numero = 10;
+
+console.log(numero);
+
+numero = 20;
+
+console.log(numero);
+
+já em const vc n pode fazer esta alteração, pq é constante.
+
+*/
+
+var soma = 9+1;
+console.log(`O valor da soma é ${soma}`)
+
