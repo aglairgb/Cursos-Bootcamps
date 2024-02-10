6b2842d01b65ed0f7706a17994ce7c6cea137299
aglairvta <aglairgb@hotmail.com>
2024-02-10 15:43:19 -0400

    Variáveis e operadores - aula

---
+++ DIO - Bootcamps/Formatação JavaScript Developer/Cursos/1. Aprenda a programar do zero com JavaScript/2. Primeiros passos com JavaScript/1. Variáveis e operadores/aula.js
@@ -0,0 +1,7 @@
+// let variavel = 10;   -> tipo de armazenamento variável.
+// const pi = 3.14;     -> tipo de armazenamento não-variável
+
+// console.log('Imprimir algo');  -> forma de impressão de mensagem.
+
+// let calculo = 1 + 1;
+// console.log(calculo);  -> Cálculo com soma.
