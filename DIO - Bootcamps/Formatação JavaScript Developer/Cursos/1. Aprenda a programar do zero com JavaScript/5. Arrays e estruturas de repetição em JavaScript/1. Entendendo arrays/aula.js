b03eadae92fabee667bf8842ad8168ec4066542e
aglairvta <aglairgb@hotmail.com>
2024-02-10 15:59:28 -0400

    Entendendo arrays - aula

---
+++ DIO - Bootcamps/Formatação JavaScript Developer/Cursos/1. Aprenda a programar do zero com JavaScript/5. Arrays e estruturas de repetição em JavaScript/1. Entendendo arrays/aula.js
@@ -0,0 +1,37 @@
+// const alunos = ['João',  'Maria', 'Pablo'];
+
+//imprime somente o primeiro aluno
+//console.log(alunos[0]);
+
+//adiciona novo item em alunos
+//alunos.push('Jorge');
+
+//adiciona novo e determina a posição
+//alunos[4] = 'Mateus';
+
+//exclui último item:
+// alunos.pop();
+
+//exclui o primeiro item:
+//alunos.shift();
+
+//exclui item selecionado:
+//alunos[0] = alunos.pop();
+
+const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+notas.push(8);
+
+//mostra o tamanho da lista:
+console.log(notas.length);
+
+
+
+
+
+   
