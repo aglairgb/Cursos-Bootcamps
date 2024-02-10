86799e1db313bc5914703d67ddfa893965a869d9
aglairvta <aglairgb@hotmail.com>
2024-02-10 15:50:29 -0400

    Funções de qualidade ao código - aula

---
+++ DIO - Bootcamps/Formatação JavaScript Developer/Cursos/1. Aprenda a programar do zero com JavaScript/3. Conhecendo funções JavaScript/1. Funções para qualidade do seu código/aula.js
@@ -0,0 +1,18 @@
+// function sayMyName(name) {
+//     console.log('Your name is: ' + name);
+// }
+
+// sayMyName('Tadeu');
+
+// function quadrado(valor) {
+//     return (valor * valor);
+// }
+// console.log(quadrado(5));
+
+// function incrementarJuros (valor, juros) {
+//     const valorAcrescimo = valor * (juros/100)
+//     return valor + valorAcrescimo;
+// }
+
+// console.log(incrementarJuros(100, 10).toFixed(2));
+
