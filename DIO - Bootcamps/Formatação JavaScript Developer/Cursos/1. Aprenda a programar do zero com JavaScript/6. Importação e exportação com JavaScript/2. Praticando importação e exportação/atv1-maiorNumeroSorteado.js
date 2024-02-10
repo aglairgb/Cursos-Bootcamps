14f5bc2e9506f371f3a735494e33ca413cdbaf2f
aglairvta <aglairgb@hotmail.com>
2024-02-10 16:08:57 -0400

    Praticando atv1

---
+++ DIO - Bootcamps/Formatação JavaScript Developer/Cursos/1. Aprenda a programar do zero com JavaScript/6. Importação e exportação com JavaScript/2. Praticando importação e exportação/atv1-maiorNumeroSorteado.js
@@ -0,0 +1,29 @@
+/* 
+1) Uma sala contém 5 alunos e para cada aluno foi sorteado um número
+de 1 - 100.
+Faça um programa que receba os 5 números sorteados para os alunos e mostre
+o maior número sorteado.
+
+Dados de entrada:
+[5, 50, 10, 98, 23]
+
+Saída
+[98]
+*/
+
+const {gets, print} = require ('./auxiliares')
+
+const quantidadeNumeros = gets();
+let maiorValor = 0;
+
+for (let i = 0; i < quantidadeNumeros; i++) {
+    const numeroSorteado = gets(); 
+    
+    if(numeroSorteado > maiorValor) {
+        maiorValor = numeroSorteado;
+    }
+}
+
+print(maiorValor);
+
+
