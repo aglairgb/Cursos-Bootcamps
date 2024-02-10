cc877bdcb283bace7b936c92e1124bf33882714a
aglairvta <aglairgb@hotmail.com>
2024-02-10 15:45:14 -0400

    Estruturas condicionais - aula

---
+++ DIO - Bootcamps/Formatação JavaScript Developer/Cursos/1. Aprenda a programar do zero com JavaScript/2. Primeiros passos com JavaScript/2. Estruturas condicionais/aula.js
@@ -0,0 +1,21 @@
+// const camisetaAzul  = true;
+// const camisetaVerde = false;
+
+// console.log (10 > 5);  true
+// console.log(5 < 2);    false
+
+// const numero = 10;
+// const par = (numero%2) == 0;
+
+// console.log(par)  condicional par/ímpar
+
+const numero = 0;
+const par = (numero%2) == 0;
+
+if (numero === 0) {
+    console.log('O número é inválido.');
+} else if (par) {
+    console.log('par');
+} else {
+    console.log('ímpar');
+}
