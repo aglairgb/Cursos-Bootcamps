cfaaac477c91366ed446702d0bc40880ca24a239
aglairvta <aglairgb@hotmail.com>
2024-06-10 16:16:28 -0400

    for

---
+++ JS & TS/Seção 3/for.js
@@ -0,0 +1,13 @@
+/* for (let i = 0; i <= 10; i++) {
+    const par = (i % 2 === 0 ? 'par' : 'impar');
+    console.log(i, par);
+} */
+
+//                 0       1          2         3        4
+const numeros = ['João', 'Maria', 'Roberto', 'Luiz', 'Carla'];
+
+
+for (let i = 0; i < numeros.length; i++) {
+    console.log('Nome: ' + numeros[i]);
+}
+
