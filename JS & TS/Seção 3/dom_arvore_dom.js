5c457c76b0b5e9a8b456dd3213336a57be44385a
aglairvta <aglairgb@hotmail.com>
2024-06-14 15:56:47 -0400

    dom_arvore_dom

---
+++ JS & TS/Seção 3/dom_arvore_dom.js
@@ -0,0 +1,10 @@
+/*
+window -> pai e mãe dos elementos
+
+árvore do DOM:
+window -> document -> html -> head -> body....
+
+Quando falo de DOM -> navegador, interface de objetos que me permitem manipular elementos de uma página.
+
+
+*/
