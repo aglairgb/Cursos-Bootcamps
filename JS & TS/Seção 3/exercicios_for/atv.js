70adca91e816ea8d4782a7ca282e12414a9211b0
aglairvta <aglairgb@hotmail.com>
2024-06-14 15:30:16 -0400

    exercicios com for

--- JS & TS/Seção 3/exercicios_for/atv.js
+++ JS & TS/Seção 3/exercicios_for/atv.js
@@ -1,22 +1,24 @@
 const elementos = [
-    {tag: 'div', texto: 'Frase 1'},      //0
-    {tag: 'footer', texto: 'Frase 2'},   //1
-    {tag: 'footer', texto: 'Frase 3'},  //2
-    {tag: 'section', texto: 'Frase 4'},  //3
+    {tag: 'div', texto: 'Frase 1'},       //0
+    {tag: 'section', texto: 'Frase 2'},   //1
+    {tag: 'main', texto: 'Frase 3'},      //2
+    {tag: 'footer', texto: 'Frase 4'},    //3
 ];
 
 const container = document.querySelector('.container');
 const div = document.createElement('div');
 
 for (let i = 0; i < elementos.length; i++) {
+    //atribuição via desestruturação de objeto
     let { tag, texto } = elementos[i];
-    let p = document.createElement(tag);
-    let textoNode = document.createTextNode(texto);
-    p.appendChild(textoNode);
-    div.appendChild(p);
+    let criarTag = document.createElement(tag);
+    criarTag.innerHTML = texto;
+
+    div.appendChild(criarTag);
 }
 
 container.appendChild(div);
+
 
 //style
 div.style.display = 'flex';
