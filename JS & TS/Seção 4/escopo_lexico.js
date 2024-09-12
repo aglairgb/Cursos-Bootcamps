078e540e54a44540941ac0c0df923faf3ac1f10e
aglairvta <aglairgb@hotmail.com>
2024-09-12 18:28:56 -0400

    escopo_lexico

---
+++ JS & TS/Seção 4/escopo_lexico.js
@@ -0,0 +1,12 @@
+/*
+o código "olha" para o ambiente de onde ele foi definido e busca a variável ou constante
+de acordo com essa estrutura hierárquica.
+*/
+
+const nome = 'Luiz';
+
+function falaNome () {
+    console.log(nome);
+}
+
+falaNome();
